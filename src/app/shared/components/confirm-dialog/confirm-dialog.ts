02cc5514a19bf795e52ecc2a9b07283d0548c798
vinipolitta <vinicius.politta1@gmail.com>
2025-07-24 16:30:05 -0300

    fix: ajuste imput

--- src/app/shared/components/confirm-dialog/confirm-dialog.ts
+++ src/app/shared/components/confirm-dialog/confirm-dialog.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -8,10 +8,12 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrl: './confirm-dialog.css'
 })
 export class ConfirmDialog {
-  // Input: Título do modal de confirmação.
-  title = input<string>('Confirmar Ação');
-  // // Input: Mensagem principal do diálogo de confirmação.
-  message = input<string>('Você tem certeza que deseja realizar esta ação?');
+  // // Input: Título do modal de confirmação.
+  // title = input<string>('Confirmar Ação');
+  @Input() title: string = 'Confirmar Ação';
+  // // // Input: Mensagem principal do diálogo de confirmação.
+  @Input() message: string = 'Você tem certeza que deseja realizar esta ação?';
+  // message = input<string>('Você tem certeza que deseja realizar esta ação?');
 
   // Injeta NgbActiveModal para permitir que o componente controle seu próprio modal (fechar, dismiss).
   constructor(public activeModal: NgbActiveModal) { }
